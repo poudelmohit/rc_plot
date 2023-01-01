4dd2130c34c681badff90baaf9bf3573ba3cabb5
Mohit Poudel <53567223+mohit254@users.noreply.github.com>
2023-01-02 00:06:12 +0545

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,20 @@
 import streamlit as st
+from RamachanDraw import fetch, phi_psi, plot
+import matplotlib.pyplot as plt
 
-st.title('🎈 App Name')
+st.set_option('deprecation.showPyplotGlobalUse', False)
 
-st.write('Hello world!')
+st.title("Ramachandran Plot Generator")
+
+# PDB id to be downloaded
+PDB_id = st.text_input("Enter the protein id(pdb id): ")
+
+# Fetch the PDB file for the given ID
+pdb_file = fetch(PDB_id)
+
+# Generate the plot using Matplotlib
+plt.figure()
+plot(pdb_file)
+
+# Display the plot in the Streamlit app using st.pyplot
+st.pyplot()
